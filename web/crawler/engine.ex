1e3e45b851be6e7429e838bc0d42af372c09c6e9
ohr486 <ohr486@gmail.com>
2016-07-13 17:05:03 +0900

    improve crawler engine

--- web/crawler/engine.ex
+++ web/crawler/engine.ex
@@ -1,30 +1,34 @@
 defmodule TokyoexHandsonDemo.Crawler.Engine do
   def get_site(url) do
-    IO.puts "Getting " <> url
+    IO.puts "### Get " <> url <> " ###"
     %{body: body} = HTTPoison.get!(url)
     body
   end
 
   def crawl(url, 0, _) do
-    IO.puts "Crawl " <> url
-    site = get_site(url) |> TokyoexHandsonDemo.Crawler.Parser.parse_html
-    TokyoexHandsonDemo.Crawler.Storage.put_page(url, site)
+    IO.puts "### Crawl " <> url <> " ###"
+    if TokyoexHandsonDemo.Crawler.Storage.has_page?(url) do
+      IO.puts "### already putted " <> url <> " ###"
+    else
+      Task.async(fn ->
+        site = get_site(url) |> TokyoexHandsonDemo.Crawler.Parser.parse_html
+        TokyoexHandsonDemo.Crawler.Storage.put_page(url, site)
+      end)
+      |> Task.await(10000)
+    end
   end
 
   def crawl(url, depth, pattern) do
-    IO.puts "Crawling " <> url <> ", depth=#{inspect depth}"
+    IO.puts "### Crawling " <> url <> ", depth=#{inspect depth} ###"
     body = get_site(url)
     site = body |> TokyoexHandsonDemo.Crawler.Parser.parse_html
+
+    # TODO: improve me 
     TokyoexHandsonDemo.Crawler.Storage.put_page(url, site)
 
-    links = TokyoexHandsonDemo.Crawler.Parser.parse_link(body, pattern)
-            |> Enum.filter(fn link ->
-              !TokyoexHandsonDemo.Crawler.Storage.has_page?(link)
-            end)
-
-    Enum.each links, fn link ->
-      task = Task.async(fn -> crawl(link, (depth - 1), pattern) end)
-      Task.await(task, 10000)
-    end
+    TokyoexHandsonDemo.Crawler.Parser.parse_link(body, pattern)
+    |> Enum.each(fn url ->
+      crawl(url, (depth - 1), pattern)
+    end)
   end
 end
--- web/crawler/storage.ex
+++ web/crawler/storage.ex
@@ -1,37 +1,37 @@
 defmodule TokyoexHandsonDemo.Crawler.Storage do
   def start_link do
-    Agent.start_link(fn -> %{} end, name: __MODULE__)
+    Agent.start_link(fn -> Map.new end, name: __MODULE__)
   end
 
-  def has_page?(name) do
+  def has_page?(url) do
     Agent.get __MODULE__, fn map ->
-      Map.has_key?(map, name)
+      Map.has_key?(map, url)
     end
   end
 
-  def put_page(name, links) do
-    IO.puts "--- Put " <> name
+  def put_page(url, pages) do
+    IO.puts "--- Put " <> url <> " ---"
     Agent.update __MODULE__, fn map ->
-      Map.put(map, name, links)
+      Map.put(map, url, pages)
     end
   end
 
   def clear_all do
-    IO.puts "--- Clear All"
+    IO.puts "--- Clear All ---"
     Agent.update __MODULE__, fn _map ->
-      %{}
+      Map.new
     end
   end
 
   def keys do
-    IO.puts "--- Show Keys"
+    IO.puts "--- Show Keys ---"
     Agent.get __MODULE__, fn map ->
       Map.keys(map)
     end
   end
 
   def dump do
-    IO.puts "Dump All"
+    IO.puts "--- Dump All ---"
     Agent.get __MODULE__, fn map ->
       map
     end
