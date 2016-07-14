5a84184433a690ee1928f6660ef9666379c2ab49
hykw <hykw1234@gmail.com>
2016-07-14 16:30:01 +0900

    support --no-branch

--- config/dev.exs
+++ config/dev.exs
@@ -11,8 +11,9 @@ config :tokyoex_handson_demo, TokyoexHandsonDemo.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../", __DIR__)]]
+  watchers: []
+#   watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
+#                     cd: Path.expand("../", __DIR__)]]
 
 
 # Watch static and templates for browser reloading.
